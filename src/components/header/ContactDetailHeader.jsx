3dd44cf0c5f6f2a7582f6c47b1427759d8d29e1b
Aayusha1523 <aayusha0360@gmail.com>
2024-06-09 23:21:22 +0545

    PIEC

---
+++ src/components/header/ContactDetailHeader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContactDetailHeader = () => {
+  return (
+    <div className="mt-3">
+      <ul
+        className="d-flex justify-content-center gap-4"
+        style={{ listStyle: "none" }}
+      >
+        <li>Head Office,Nepal Office:-Bagbazar,Kathmandu</li>
+        <li>+977 9823582134, 01-5909940</li>
+        <li>Email:nepla@pinteducation.com</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactDetailHeader;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import NavbarBar from "../components/header/NavbarBar";
+import ContactDetailHeader from "../components/header/ContactDetailHeader";
+import Logo from "../components/header/Logo";
+import style from "../style/header.module.css";
+import Banner from "../components/banner/Banner";
+const Header = () => {
+  return (
+    <section>
+      {/* this is first section  */}
+      <section id="first-section">
+        <div className={style.logo}>
+          <Logo />
+        </div>
+        <div>
+          <ContactDetailHeader />
+        </div>
+        <div>
+          <NavbarBar />
+        </div>
+      </section>
+      {/* this section 2  */}
+      <section>
+        <Banner />
+      </section>
+    </section>
+  );
+};
+
+export default Header;
