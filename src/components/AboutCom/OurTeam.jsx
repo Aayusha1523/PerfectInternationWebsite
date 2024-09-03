449dfde759932aee3d2a6bd9d8b7f431fdcf10f3
ROHAN-88 <therohanshrestha6655@gmail.com>
2024-09-03 16:41:14 +0545

    comit problem resolve

--- src/components/AboutCom/OurTeam.jsx
+++ src/components/AboutCom/OurTeam.jsx
@@ -1,57 +1,28 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
-import LogoI from "../../assets/Images/Perfect logo.jpg"
-
+import LogoI from "../../assets/Images/Perfect logo.jpg";
+import teamInfo from "../about/teamInfo";
 
 const OurTeam = () => {
   return (
     <Container>
       <h1 style={{ alignItems: "center" }}>OUR TEAM</h1>
       <Row>
-        <Col className="people" lg="2" md="2">
-          <img
-            style={{ width: "200px", padding: "15px" }}
-            // src={Vision}
-            src={LogoI}
-          ></img>
-          <h4>Director</h4>
-          <h6>Subash Pandey</h6>
-          <p>Reading,Uk</p>
-        </Col>
-
-        <Col className="people" lg="2" md="2">
-          <img
-            style={{ width: "200px", padding: "15px" }}
-            // src={Vision}
-            src={LogoI}
-          ></img>
-          <h4>Director</h4>
-          <h6>Laxmi Poudel</h6>
-          <p>Reading,Uk</p>
-        </Col>
-
-        <Col className="people" lg="2" md="2">
-          <img
-            style={{ width: "200px", padding: "15px" }}
-            // src={Vision}
-            src={LogoI}
-          ></img>
-          <h4>Director</h4>
-          <h6>Subash Pandey</h6>
-          <p>Reading,Uk</p>
-        </Col>
-
-        <Col className="people" lg="2" md="2">
-          <img
-            style={{ width: "200px", padding: "15px" }}
-            // src={Vision}
-            src={LogoI}
-          ></img>
-          <h4>Director</h4>
-          <h6>Subash Pandey</h6>
-          <p>Reading,Uk</p>
-        </Col>
+        {teamInfo.map((item) => {
+          return (
+            <Col className="people" lg="2" md="2" key={item.id}>
+              <img
+                style={{ width: "200px", padding: "15px" }}
+                // src={Vision}
+                src={LogoI}
+              ></img>
+              <h4>{item.postion}</h4>
+              <h6>{item.name}</h6>
+              <p>Reading,Uk</p>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,17 @@
 import "../style/about.css";
 
 import { Col, Container, Row } from "react-bootstrap";
-import { ReactTyped } from "react-typed";
 
-import Goal from "../assets/Images/goal.webp";
-import Mission from "../assets/Images/missi.webp";
-import Logo from "../assets/Images/Perfect logo.jpg";
-import Vision from "../assets/Images/vision.jpeg";
-
+import LogoP from "../assets/Images/PICElogo.jpeg";
+import AboutMission from "../components/AboutCom/AboutMission";
+import Goal from "../components/AboutCom/Goal";
+import OurTeam from "../components/AboutCom/OurTeam";
+import TypedWords from "../components/AboutCom/TypedWords";
+import Vision from "../components/AboutCom/Vision";
 const About = () => {
   return (
     <>
-      <div className="blurbox mb-5">
-        <div className="Banner"></div>
-        <div className="typeds">
-          <ReactTyped
-            strings={[
-              "ABOUT US",
-              "Perfect International Education Consultancy",
-            ]}
-            typeSpeed={40}
-            loop
-          />
-        </div>
-      </div>
+      <TypedWords />
       {/* THE INTRODUCTION ALONG WITH LOGO */}
       <Container>
         <Row>
@@ -50,7 +38,7 @@ const About = () => {
                 width: "200px",
                 height: "200px",
               }}
-              src={Logo}
+              src={LogoP}
             />
           </Col>
         </Row>
@@ -58,184 +46,14 @@ const About = () => {
 
       {/* Intro Ended Here */}
       {/* MISSION STARTED */}
-      <div className="Mission">
-        <Container>
-          <Row style={{ padding: "30px" }}>
-            <Col>
-              {/* For IMAGE */}
-              <img
-                style={{ width: "400px", height: "365px", paddingTop: "110px" }}
-                src={Mission}
-              ></img>
-            </Col>
-
-            <Col className="Intro">
-              <h4 style={{ color: "darkblue", fontSize: "44px" }}>
-                OUR MISSION
-              </h4>
-
-              <div className="card">
-                <p
-                  style={{
-                    paddingTop: "6px",
-                    fontSize: "18px",
-                    textAlign: "justify",
-                    zIndex: "2",
-                  }}
-                >
-                  Our mission is to provide comprehensive and personalized
-                  consultancy services that ensure each student finds the
-                  perfect educational opportunity fitting their aspirations and
-                  capabilities.We value the uniqueness of each students
-                  ambitions and endeavors to provide tailored solutions that
-                  enhance their academic and personal growth. By fostering
-                  relationships with students, families, and educational
-                  institutions, we create enduring partnerships that support
-                  lifelong learning and success. Ultimately, our mission is to
-                  inspire and empower students to achieve their educational
-                  aspirations abroad, thereby shaping a brighter future for
-                  themselves and contributing positively to global communities.
-                  Perfect International Education Consultancy is not just about
-                  sending students abroadits about guiding them towards a path
-                  of academic excellence and personal fulfillment.
-                </p>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-        {/* MISSION ENDED */}
-      </div>
+      <AboutMission />
       {/* VISION */}
-      <div style={{ paddingTop: "2rem" }} className="vision">
-        <Container>
-          <Row>
-            <Col className="Intro" lg="7" md="3">
-              <h4 style={{ color: "darkblue", fontSize: "44px" }}>VISION</h4>
-              <p
-                style={{
-                  paddingTop: "6px",
-                  fontSize: "18px",
-                  textAlign: "justify",
-                }}
-              >
-                {" "}
-                Our vision is to be a beacon of excellence in international
-                education consultancy, guiding students towards academic success
-                and personal growth. By bridging the gap between ambitious
-                students and world-class educational institutions, we aim to
-                create a network of global citizens equipped with the knowledge,
-                skills, and cultural awareness necessary to thrive in an
-                increasingly interconnected world.
-              </p>
-            </Col>
-            <Col gx="1" md="2"></Col>
-
-            <Col lg="2" md="2">
-              {/* For IMAGE */}
-              <img
-                className=" Vision"
-                style={{ width: "300px", height: "300px", paddingTop: "50px" }}
-                src={Vision}
-              ></img>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Vision />
       {/* VISION ENDS HERE */}
       {/* GOAL STARTED */}
-      <div className="Mission">
-        <Container>
-          <Row>
-            <Col md="12" lg="6">
-              {/* For IMAGE */}
-              <img
-                style={{ width: "400px", height: "365px", paddingTop: "110px" }}
-                // src={Goal}
-                src={Goal}
-              ></img>
-            </Col>
-
-            <Col className="Intro" md="12" lg="6">
-              <h4 style={{ color: "darkblue", fontSize: "44px" }}>OUR GOAL</h4>
-
-              <div className="card">
-                <p
-                  style={{
-                    paddingTop: "6px",
-                    fontSize: "18px",
-                    textAlign: "justify",
-                    zIndex: "999",
-                  }}
-                >
-                  Perfect International Education is dedicated to realizing its
-                  vision through a set of well-defined goals that drive our
-                  consultancy services. These goals reflect our commitment to
-                  excellence, student success, and global engagement:To provide
-                  personalized and comprehensive guidance tailored to each
-                  students unique academic and career aspirations,To facilitate
-                  access to top-tier educational institutions in the UK, USA,
-                  Australia, Canada, and New Zealand. To ensure students are
-                  well-prepared for the academic rigor and cultural experiences
-                  of studying abroad.To prepare students for the cultural
-                  differences and social dynamics they will encounter abroad. To
-                  promote cross-cultural understanding and global citizenship
-                  through various programs and initiatives.
-                </p>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-        {/* GOAL ENDED */}
-      </div>
+      <Goal />
       {/* Our team */}
-      <Container>
-        <h1 style={{ alignItems: "center" }}>OUR TEAM</h1>
-        <Row>
-          <Col className="people" lg="2" md="2">
-            <img
-              style={{ width: "200px", padding: "15px" }}
-              // src={Vision}
-              src={Logo}
-            ></img>
-            <h4>Director</h4>
-            <h6>Subash Pandey</h6>
-            <p>Reading,Uk</p>
-          </Col>
-
-          <Col className="people" lg="2" md="2">
-            <img
-              style={{ width: "200px", padding: "15px" }}
-              // src={Vision}
-              src={Logo}
-            ></img>
-            <h4>Director</h4>
-            <h6>Laxmi Poudel</h6>
-            <p>Reading,Uk</p>
-          </Col>
-
-          <Col className="people" lg="2" md="2">
-            <img
-              style={{ width: "200px", padding: "15px" }}
-              // src={Vision}
-              src={Logo}
-            ></img>
-            <h4>Director</h4>
-            <h6>Subash Pandey</h6>
-            <p>Reading,Uk</p>
-          </Col>
-
-          <Col className="people" lg="2" md="2">
-            <img
-              style={{ width: "200px", padding: "15px" }}
-              // src={Vision}
-              src={Logo}
-            ></img>
-            <h4>Director</h4>
-            <h6>Subash Pandey</h6>
-            <p>Reading,Uk</p>
-          </Col>
-        </Row>
-      </Container>
+      <OurTeam />
       {/* Ends Here */}
     </>
   );
